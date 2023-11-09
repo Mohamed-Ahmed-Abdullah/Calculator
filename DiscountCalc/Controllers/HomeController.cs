5314c13f52bd3206509262f315d0510bd756c465
Mohamed Ahmed Abdullah <mohamed.1234@hotmail.com>
2023-11-10 11:01:23 +1300

    init

---
+++ DiscountCalc/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using DiscountCalc.Models;
+using DiscountCalc.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DiscountCalc.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ICalculateService _calculateService;
+        public HomeController(ILogger<HomeController> logger,
+            ICalculateService calculateService)
+        {
+            _logger = logger;
+            _calculateService = calculateService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult MyPostAction([FromForm] string inputData)
+        {
+            var result = _calculateService.Add(1,2);
+            // Process the posted data and return a response
+            return Json($"Data received successfully inputData={inputData} calcResult={result}");
+        }
+    }
+}
---
+++ DiscountCalc/Services/CalculateService.cs
@@ -0,0 +1,31 @@
+﻿namespace DiscountCalc.Services
+{
+    public interface ICalculateService
+    {
+        int Add(int a, int b);
+    }
+
+    public class CalculateService : ICalculateService, IDisposable
+    {
+        public CalculateService()
+        {
+            Console.WriteLine("CalculateService instance created");
+        }
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public void Dispose()
+        {
+            Console.WriteLine("CalculateService instance disposed");
+        }
+
+        ~CalculateService()
+        {
+            Console.WriteLine("CalculateService instance finalized");
+        }
+    }
+
+}
