409128fe9c1424edba9061641a9e5fee581cdd8c
Mohamed Ahmed Abdullah <mohamed.1234@hotmail.com>
2023-11-10 11:31:55 +1300

    adding post and ajax methods

--- DiscountCalc/wwwroot/js/site.js
+++ DiscountCalc/wwwroot/js/site.js
@@ -2,3 +2,43 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    //$("#myForm").submit(function (event) {
+    //    event.preventDefault();
+
+    //    // Perform POST request using jQuery
+    //    $.post({
+    //        url: $(this).attr("action"),
+    //        data: $(this).serialize(),
+    //        dataType: 'json',
+    //        success: function (data) {
+    //            console.log(data);
+    //            // Handle the response as needed
+    //        },
+    //        error: function (error) {
+    //            console.error('Error calling controller action:', error);
+    //        }
+    //    });
+    //});
+
+    $(document).ready(function () {
+        $("#myForm").submit(function (event) {
+            event.preventDefault();
+
+            // Perform POST request using jQuery $.ajax()
+            $.ajax({
+                type: "POST",
+                url: $(this).attr("action"),
+                data: $(this).serialize(),
+                dataType: 'json',
+                success: function (data) {
+                    console.log(data);
+                    // Handle the response as needed
+                },
+                error: function (error) {
+                    console.error('Error calling controller action:', error);
+                }
+            });
+        });
+    });
+});
